d37a19d7694b1e2e015a0e3d4543dd5d71d9ad8b
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-06-02 00:11:07 +0600

    project setup

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Main from './Layout/Main.jsx'
+import Home from './Pages/Home/Home'
+const router = new createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
